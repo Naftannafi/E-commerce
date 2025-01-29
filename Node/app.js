ce5a03965f37b8f3c59f8136cc0b9f31dbd0c3e8
Naftannafi <149290646+Naftannafi@users.noreply.github.com>
2025-01-29 23:03:37 +0300

    Delete Node directory

--- Node/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const PORT = process.env.PORT || 3500;
-
-app.get("/", (req, res) => {
-  res.sendFile("/Frontend/index.html", { root: __dirname });
-});
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
